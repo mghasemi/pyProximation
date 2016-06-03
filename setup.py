9c82ee59553719a2d6819d5bee59f634c5ea2ea0
mghasemi <mehdi.ghasemi@gmail.com>
2016-06-03 11:25:07 -0600

    Renamed the project

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 Description = """This package provides basic operations with measures,\
-             orthogonal system of functions and solving integro-differential equations. """
+             orthogonal system of functions, interpolation and solving integro-differential equations. """
 
 setup(
-    name = 'IntgDiff',
-    version = '1.0.1',
+    name = 'ApproxPy',
+    version = '1.0.0',
     author = 'Mehdi Ghasemi',
     author_email = 'mehdi.ghasemi@gmail.com',
-    packages = ['IntgDiff'],
-    #url = 'https://github.com/mghasemi/CvxAlgGeo.git',
+    packages = ['ApproxPy'],
+    #url = 'https://github.com/mghasemi/ApproxPy.git',
     license = 'MIT License',
     description = Description,
     #long_description = open('README.txt').read(),
